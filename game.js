bc22dd5bc49ebce9d51c9f2c256f0c86ab92e6e2
Morgan Hartman <littlemiss.hartman@gmail.com>
2019-03-28 23:16:03 -0600

    game done

--- game.js
+++ game.js
@@ -1,62 +1,6 @@
-let CardTypes = [
-	{
-		id: "obedience",
-		content: "Obedience",
-	},
-	{
-		id: "obedience",
-		content: "1 Nep 3:7"
-	},
-	{
-		id: "theFall",
-		content: "Fall of Adam",
-	},
-	{
-		id: "theFall",
-		content: "2 Nep 2:25"
-	},
-	{
-		id: "agency",
-		content: "Agency",
-	},
-	{
-		id: "agency",
-		content: "2 Nep 2:27"
-	},
-	{
-		id: "righteousness",
-		content: "Righteousness",
-	},
-	{
-		id: "righteousness",
-		content: "2 Nep 9:28-29"
-	},
-	// {
-	// 	topic: "Natural Man",
-	// 	reference: "Mosiah 3:19"
-	// },
-	// {
-	// 	topic: "Scriptures",
-	// 	reference: "2 Nep 32:3"
-	// },
-];
-
-let shuffleCards = () => {
-	// let copy = CardTypes.concat(CardTypes);
-	return CardTypes.sort(() => 0.5 - Math.random());
-}
-
 let app = new Vue({
 	el: '#app',
 	data: {
-		showLogin: true,
-		loggedIn: false,
-		loginType: "email",
-		loginInput: "",
-		username: "",
-		email: "",
-		emailMessage: "",
-		showEmailMessage: false,
 		timeElapsed: 0,
 		startTime: 0,
 		duration: 0,
@@ -65,92 +9,131 @@ let app = new Vue({
 		disableClick: false,
 		secondFlip: false,
 		cards: [],
-		prevCardIndex: 0,
+		prevCardIndex: null,
 		cardClasses: {},
-		playingGame: false
+		playingGame: false,
+		gameDifficulty: "Easy",
+		gameType: "scriptureMastery",
+		cardTypes: {
+			"scriptureMasteryEasy" : [
+				{
+					id: "obedience",
+					content: "I will go and do",
+				},
+				{
+					id: "obedience",
+					content: "1 Nep 3:7"
+				},
+				{
+					id: "theFall",
+					content: "The Fall brings joy",
+				},
+				{
+					id: "theFall",
+					content: "2 Nep 2:25"
+				},
+				{
+					id: "agency",
+					content: "We are free to choose",
+				},
+				{
+					id: "agency",
+					content: "2 Nep 2:27"
+				},
+				{
+					id: "righteousness",
+					content: "Being learned is good if you follow God",
+				},
+				{
+					id: "righteousness",
+					content: "2 Nep 9:28-29"
+				}
+			],
+			"scriptureMasteryHard" : [
+				{
+					id: "obedience",
+					content: "Obedience",
+				},
+				{
+					id: "obedience",
+					content: "1 Nep 3:7"
+				},
+				{
+					id: "theFall",
+					content: "The Fall",
+				},
+				{
+					id: "theFall",
+					content: "2 Nep 2:25"
+				},
+				{
+					id: "agency",
+					content: "Agency",
+				},
+				{
+					id: "agency",
+					content: "2 Nep 2:27"
+				},
+				{
+					id: "learned",
+					content: "Righteousness",
+				},
+				{
+					id: "learned",
+					content: "2 Nep 9:28-29"
+				},
+				{
+					id: "christ",
+					content: "Christ brings the remission of sins",
+				},
+				{
+					id: "christ",
+					content: "2 Nep 25:23, 26"
+				},
+				{
+					id: "truth",
+					content: "Ask to know the truth",
+				},
+				{
+					id: "truth",
+					content: "Mor 10: 4, 5"
+				},
+				{
+					id: "faith",
+					content: "Faith is things hoped for",
+				},
+				{
+					id: "faith",
+					content: "Ether 12:6"
+				},
+				{
+					id: "humility",
+					content: "Weak things become strong",
+				},
+				{
+					id: "humility",
+					content: "Ether 12:27"
+				}
+			],
+			"placesEasy" : [
+
+			],
+			"placesHard" : [
+
+			]
+		}
 	},
 	computed: {
 		totalPairs: function () {
 			return this.cards.length();
+		},
+		cardArray: function() {
+			return this.gameType + this.gameDifficulty;
 		}
 	},
 	methods: {
-		logInWithEmail: function() {
-			this.loginType = "email";
-			this.showInput = true;
-		},
-		logInWithUsername: function() {
-			this.emailMessage = "";
-			this.loginType = "username";
-			this.showInput = true;
-		},
-		closeLogin: function() {
-			this.showLogin = false;
-			this.emailMessage = "";
-			this.showEmailMessage = false;
-		},
-		closeLoginBecauseLoggedIn: function() {
-			if(this.loggedIn){
-				this.showLogin = false;
-				this.emailMessage = "";
-				this.showEmailMessage = false;
-			}
-		},
-		openLogin: function() {
-			this.showLogin = true;
-		},
-		getLoggedIn: function() {
-			this.loggedIn = true;
-		},
-		handleInput: async function() {
-			if(this.loginType === "username") {
-				this.username = this.loginInput;
-				if (this.username === "morgan_hartman") {
-					this.loggedIn = true;
-					this.closeLoginBecauseLoggedIn();
-				}
-			}
-			else if (this.loginType === "email") {
-				await this.processAPICall();
-				this.$nextTick(this.closeLoginBecauseLoggedIn());
-			} else {
-				console.log('error');
-				this.emailMessage = "Please enter a valid email address or username."
-				this.loggedIn = false;
-				this.showEmailMessage = true;
-			}
-		},
-		logOut: function() {
-			this.loggedIn = false;
-			this.loginInput = "";
-			this.username = "";
-		},
-		processAPICall: async function() {
-			var _this = this;
-			this.email = this.loginInput;
-			console.log(`Email Input is ${this.email}.`);
-			const accessKey = "81a42f979a60a5020baa7b1ec7c058e5";
-			const url = "http://apilayer.net/api/check?access_key=" + accessKey + "&email=" + this.email;
-			await fetch(url)
-				.then(function(response) {
-					return response.json();
-				}).then(function(json) {
-					console.log(json);
-					if (json.score >= 0.8 || json.format_valid === true) {
-						console.log("success");
-						_this.emailMessage = "Valid";
-						_this.username = _this.email;
-						_this.loggedIn = true;
-					} else {
-						console.log("fail");
-						_this.emailMessage = "Please enter a valid email address.";
-						_this.loggedIn = false;
-					}
-				});
-			this.showEmailMessage = true;
-		},
 		resetGame() {
-			this.cards = shuffleCards();
+			//this.cards = this.shuffleCards();
 			this.cards.forEach((card) => {
 				this.$set(card, 'flipped', false);
 				this.$set(card, 'found', false);
@@ -159,7 +142,7 @@ let app = new Vue({
 			this.turns = 0;
 			this.matchedPairs = 0;
 			this.secondFlip = false;
-			this.prevCardIndex = 0;
+			this.prevCardIndex = null;
 			this.duration = 0;
 			this.playingGame = false;
 		},
@@ -170,7 +153,8 @@ let app = new Vue({
 				this.timer(1000);
 			}
 			//is second flip
-			if (card === this.cards[this.prevCardIndex]) {
+			console.log(this.cards.indexOf(card), this.prevCardIndex);
+			if (this.cards.indexOf(card) === this.prevCardIndex) {
 				console.log("Pick a different card.");
 				return;
 			}
@@ -214,7 +198,7 @@ let app = new Vue({
 		endGame() {
 			let end = new Date();
 
-			alert(`You Won! You completed the game in: ${ Math.round((end - this.startTime) / 1000) } seconds`);
+			alert(`You Won! You completed the game in: ${ Math.round((end - this.startTime) / 1000) } seconds and ${this.turns} turns`);
 			this.resetGame();;
 		},
 		incrementTime() {
@@ -225,66 +209,25 @@ let app = new Vue({
 			this.timer(1000);
 		},
 		timer(milliseconds) {
-			if (!this.playingGame) {
-				return;
-			}
 			t = setTimeout(this.incrementTime, milliseconds);
 		},
-		async uploadPlayer() {
-			try {
-				const formData = new FormData();
-				formData.append('photo', this.file, this.file.name)
-				let res = await axios.post('/api/players', {
-					name: req.body.name,
-					turns: req.body.turns,
-					duration: req.body.duration,
-					date: req.body.date,
-				});
-				this.addItem = res.data;
-			} catch (error) {
-				console.log(error);
-			}
+		shuffleCards() {
+			this.cards = this.cardTypes[this.cardArray].sort(() => 0.5 - Math.random());
+			//return this.cardTypes[this.cardArray].sort(() => 0.5 - Math.random());
 		},
-		async getplayers() {
-			try {
-				let res = await axios.get("/api/players");
-				this.players = res.data;
-				return true;
-			} catch (error) {
-				console.log(error);
-			}
+		setGameType(gameType) {
+			console.log("changing game type to " + gameType);
+			this.gameType = gameType;
+			this.shuffleCards();
 		},
-		selectItem(item) {
-			console.log(item._id)
-			this.findTitle = "";
-			this.findItem = item;
-		},
-		async deleteItem(item) {
-			try {
-				console.log(item._id)
-				let response = await axios.delete("/api/players/" + item._id);
-				this.findItem = null;
-				this.getplayers();
-				return true;
-			} catch (error) {
-				console.log(error);
-			}
-		},
-		async editItem(item) {
-			try {
-				let response = await axios.put("/api/players/" + item._id, {
-					title: this.findItem.title,
-				});
-				this.findItem = null;
-				this.getplayers();
-				return true;
-			} catch (error) {
-				console.log(error);
-			}
-		},
+		setGameDifficulty(gameDifficulty) {
+			console.log("changing game type to " + gameDifficulty);
+			this.gameDifficulty = gameDifficulty;
+			this.shuffleCards();
+		}
 	},
 	created() {
+		this.shuffleCards();
 		this.resetGame();
-		this.showLogin = true;
 	},
 })
